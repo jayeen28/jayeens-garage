c0bf84682838c508f1ac57ab5fdc9a9dc1a661c2
MD. Jayeen Bin Alam <jayeen6783@gmail.com>
2021-10-04 15:34:05 +0600

    simple issue fixed

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Header.css'
 import logo from '../../images/jayeens-garage-logo.png';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Nav, Navbar } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import Button from '@restart/ui/esm/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -12,19 +12,21 @@ const Header = () => {
         <header className="header-section">
             <Navbar className="jg-nav">
                 <div className="container">
-                    <Navbar.Brand href="/home">
-                        {/* nav bar brand */}
-                        <img
-                            src={logo}
-                            width="100"
-                            height="80"
-                            className="img-fluid"
-                            alt="Jayeens garage logo"
-                        />
-                        <span className="jg-bname">
-                            <span style={{ color: '#FDBD4E' }}>Jayeen<span style={{ color: '#FFFFFF' }}>'</span>s</span> <span style={{ color: '#FFFFFF' }}>Garage</span>
-                        </span>
-                    </Navbar.Brand>
+                    <NavLink to="/home">
+                        <Navbar.Brand>
+                            {/* nav bar brand */}
+                            <img
+                                src={logo}
+                                width="100"
+                                height="80"
+                                className="img-fluid"
+                                alt="Jayeens garage logo"
+                            />
+                            <span className="jg-bname">
+                                <span style={{ color: '#FDBD4E' }}>Jayeen<span style={{ color: '#FFFFFF' }}>'</span>s</span> <span style={{ color: '#FFFFFF' }}>Garage</span>
+                            </span>
+                        </Navbar.Brand>
+                    </NavLink>
                     {/*navbar links*/}
                     <Nav className="jg-nav-links">
                         <NavLink className="jg-nav-link" to="/home">Home</NavLink>
