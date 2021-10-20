823c30ab12dcfc5e80b86aad5c53830545b7ce0e
MD. Jayeen Bin Alam <jayeen6783@gmail.com>
2021-10-20 21:09:58 +0600

    api added

--- src/Components/Signin/Signin.js
+++ src/Components/Signin/Signin.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Signin = () => {
+    const { register, handleSubmit = useForm();
+    const onSubmit = (data) => {
+        console.log(data);
+    };
     return (
         <div>
-            sign in page
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input defaultValue="test" {...register("example")} />
+
+                <input {...register("exampleRequired", { required: true })} />
+                {errors.exampleRequired && <p>This field is required</p>}
+
+                <input type="submit" />
+            </form>
         </div>
     );
 };
